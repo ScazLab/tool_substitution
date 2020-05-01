e84a190837a4b089e282fdfa9566fe7577d19c47
MeiyingQin <meiyingqin@gmail.com>
2020-05-01 19:47:29 -0400

    [WIP] meiying's code, need to be debugged

--- pointcloud_bb.py
+++ pointcloud_bb.py
@@ -158,21 +158,97 @@ class BoundingBox(object):
     def _project_on_to_pcs(self, components):
         pass
 
-    def _inverse_pc_transform(self, axis):
+    def _inverse_pc_transform(self, bb2D, b3):
         # Meiying
-        pass
+        b3 = b3 / np.linalg.norm(b3) # normalize
 
+        pnts_1d = self.pnts.dot(b3)
+        
+        # initial 2D bb is located in the center of the point cloud.
+        # We want to copy it and shift both in the directions of 3rd PC
+        
+        width = pnts_1d.max()  - pnts_1d.min()
+        width_dir = b3 * width / 2.0 # This tells us how to translate points
+        
+        # width_dir = b3 * pca3D.singular_values_[2] / 2
+        
+        bb3D = self.pca2D.inverse_transform(bb2D)
+        
+        bb_side1 = bb3D + width_dir
+        bb_side2 = bb3D - width_dir
+        
+        bb3D = np.vstack([bb_side1, bb_side2])
+        
+        return bb3D
+
+    def _bounding_box_to_normalized_axis(self, bb):
+        # Meiying
+        # bb is 10 by 3. The 5th and 10th one is a repetition
+        unnormialzied_axis = self._bounding_box_to_axis(bb)
+        return unnormialzied_axis / np.linalg.norm(unnormialzied_axis, axis=1, keepdims=True)
+    
+    def _bounding_box_to_axis(self, bb):
+        # Meiying
+        # bb is 10 by 3. The 5th and 10th one is a repetition
+        dtype = [('x', 'f8'), ('y', 'f8'), ('z', 'f8'), ('length', 'f8')]
+        axis_with_length = np.array([(bb[0, i], bb[1, i], bb[2, i], np.linalg.norm(bb[:, i])) for i in range(3)], dtype = dtype)
+        axis_with_length.sort(order = 'length')
+        return axis_with_length[:, :3].T
+        
+    def _get_bounding_box(self, axis, projection_indices, other_index):
+        # Meiying
+        pnts = self._project_on_to_pcs(self.pca3D.components_[indices[(projection_indices], indices[projection_indices]), :]) # test and fix this
+        bb2D = self.mbb2D(pnts)
+        bb3D = self._inverse_pc_transform(bb2D, self.pca3D.components_[other_index, :])
+        return bb3D
+
+    def _get_bounding_box_area(self, bounding_box):
+        # Meiying
+        axes = self._bounding_box_to_axis(bounding_box)
+        return np.linalg.norm(axes[:, 0]) * np.linalg.norm(axes[:, 1]) * np.linalg.norm(axes[:, 2])
+    
+    def _close_to(m, n, error=1e-6):
+        return m >= n - error and m <= n + error    
+    
+    def _get_bounding_box_parameter(self, bounding_box):
+        # Meiying
+        axes = self._bounding_box_to_axis(bounding_box)
+        return 2 * (np.linalg.norm(axes[:, 0]) + np.linalg.norm(axes[:, 1]) + np.linalg.norm(axes[:, 2]))
+    
+    def _is_same_bounding_boxes(self, bounding_boxes):
+        self._close_to(np.linalg.norm(bounding_boxes - bounding_boxes[:, 0]), 0, error=-0.01)
+
+    def _choose_bounding_box(self, boxes):
+        dtype = [('index', 'i4'), ('area', 'f8'), ('parameter', 'f8')]
+        data = np.array([(i, self._get_bounding_box_area(boxes[:, :, i]), self._get_bounding_box_parameter(boxes[:, :, i])) for i in range(3)], dtype = dtype)
+        
+        min_area_bbs = data[data['area'] < data['area'][0] * 1.05]
+        min_area_bbs.sort(order='parameter')
+        
+        index = min_area_bbs['index'][0]
+        
+        return boxes[:, :, index]
+        
     def main(self):
         # Meiying
         found_box = False
-        current_axis = [self.pca3D.components_[0,:], self.pca3D.components_[1,:], self.pca3D.components_[2,:]]
-        While not found_box:
-            pass
-    
-    def _bounding_box_to_axis(self, bb):
-        # bb is 10 by 3. The 5th and 10th one is a repetition
-        pass
-
+        box = None
+        #current_axis = [self.pca3D.components_[0,:], self.pca3D.components_[1,:], self.pca3D.components_[2,:]]
+        while not found_box:
+            box_1 = self._get_bounding_box(current_axis, [0, 1], 2)
+            box_2 = self._get_bounding_box(current_axis, [0, 2], 1)
+            box_3 = self._get_bounding_box(current_axis, [1, 2], 0)
+            boxes = np.array([box_1, box_2, box_3])
+            found_box = self._is_same_bounding_boxes(boxes)
+            if found_box:
+                found_box = True
+                box = np.mean(boxes, axis=3) # test and fix This
+            else:
+                box = self._choose_bounding_box(boxes)
+                current_axis = self._bounding_box_to_normalized_axis(box)
+        
+        return box, self._bounding_box_to_axis(box)
+                
 
     def mbb3D(self):
         """
@@ -202,7 +278,13 @@ class BoundingBox(object):
         bb3D = np.vstack([bb_side1, bb_side2])
     
         return bb3D
-
+    
+    def _visualize_point_cloud(self, fig, axis):
+        axis.scatter(xs=self.pnts[:,0], ys=self.pnts[:,1], zs=self.pnts[:,2], c='b')
+    
+    def _visualize_bounding_box(self, fig, axis, bb, color='r'):
+        ax.scatter(xs=bb[:,0], ys=bb[:,1], zs=bb[:,2], c=color, s=100)
+        
     def plot_bb(self, n="3D", ax=None):
         """Visualize points and bounding box"""
 
