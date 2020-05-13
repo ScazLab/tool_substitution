0b6dcd651c3512cd812152e286e91d2e7f34895b
Jake Brawer <jake.brawer@yale.edu>
2020-05-13 13:19:06 -0400

    Method for determining correct substitute tool oritentation.

--- compare_tools.py
+++ compare_tools.py
@@ -13,6 +13,21 @@ from scipy.spatial import ConvexHull
 FIGS_PATH = "./figs/"
 
 
+
+def pnts_to_img(pnts, fn=None, dims=(3,3)):
+    fig, ax = plt.subplots(figsize=dims)
+
+    print(pnts.shape)
+    ax.scatter(x=pnts[:, 0], y=pnts[:, 1], c='b', s=50)
+    plt.axis('off')
+    plt.axis("tight")  # gets rid of white border
+    plt.axis("image")  # square up the image instead of filling the "figure" space
+
+    if fn is None:
+        plt.show()
+    else:
+        plt.savefig("{}{}".format(FIGS_PATH, fn))
+
 def bbs_to_img(bbs, fn=None, dims=(3,3)):
     fig, ax = plt.subplots(figsize=dims)
 
@@ -33,11 +48,71 @@ def bbs_to_img(bbs, fn=None, dims=(3,3)):
 
 class CompareTools(object):
 
-    def __init__(self):
+    def __init__(self, src_tool_pc, sub_tool_pc):
         "Class for comparing shapes of two tools."
 
+        self.src_pc = src_tool_pc
+        self.sub_pc = sub_tool_pc
 
-    def hamming_distance(self, img1_fn, img2_fn, width=300):
+
+    def choose_tool_orientation(self, metric='hamming', width=300):
+
+        if metric == 'hamming':
+            dist_measure = self.hamming_distance
+        elif metric == 'moment':
+            dist_measure = self.moments_distance
+
+        fns  = ['src.png', 'sub.png']
+
+        for pc, fn in zip([self.src_pc, self.sub_pc], fns):
+            _,bb = pc.bb_2d_projection([0, 1], 2, visualize=False)
+            pnts = bb.get_pc()
+
+            pnts_to_img(pnts, fn=fn)
+
+        print("METRIC: {}".format(metric))
+
+        for flip_sub in [False, True]:
+
+            sub_status = "flipped" if flip_sub else "unflipped"
+            score = dist_measure(fns[0], fns[1], flip_sub, width)
+
+            print("{} score: {}".format(sub_status, score))
+
+    def moments_distance(self, img1_fn, img2_fn, flip_sub=False,width=300):
+        """
+        Uses normalized central moments (ncm) to compare 
+        """
+
+        src_shape, sub_shape = self._resize_and_norm_imgs(img1_fn,
+                                                          img2_fn,
+                                                          flip_sub,
+                                                          width)
+
+
+
+        cv2.imshow('tool1', src_shape)
+        cv2.waitKey()
+        cv2.imshow('tool2', sub_shape)
+        cv2.waitKey()
+
+
+        moments1 = cv2.moments(src_shape)
+        moments2 = cv2.moments(sub_shape)
+
+        #central normalized moments:
+        # double  nu20, nu11, nu02, nu30, nu21, nu12, nu03
+        ncm_keys = [m for m in moments1 if 'nu' in m]
+
+        ncm1 = np.array([moments1[m] for m in ncm_keys])
+        ncm2 = np.array([moments2[m] for m in ncm_keys])
+
+        score = np.linalg.norm(ncm1 - ncm2)
+        print("MOMENTS SCORE: {}".format(score))
+
+        return score
+
+    def hamming_distance(self, img1_fn, img2_fn, flip_sub, width=300):
         """
         Similarity metric for two 2D shapes. Calculates the area of non-overlap
         between two contours. The larger the overlap, the more similar shapes are.
@@ -45,7 +120,48 @@ class CompareTools(object):
 
         """
 
-        # Read in image as grayscale
+        src_shape, sub_shape = self._resize_and_norm_imgs(img1_fn,
+                                                          img2_fn,
+                                                          flip_sub,
+                                                          width=width)
+        # sub_shape = np.flip(sub_shape)
+
+        cv2.imshow('tool1', src_shape)
+        cv2.waitKey()
+        cv2.imshow('tool2', sub_shape)
+        cv2.waitKey()
+
+
+        # Shapes are drawn in white ( white == 1)
+        shape1_area = np.count_nonzero(src_shape)
+        shape2_area = np.count_nonzero(sub_shape)
+        print("shape 1 area: {} shape 2 area: {}".format(shape1_area, shape2_area))
+
+        # Intersection area == 2, black == 0, contour area == 1
+        score = np.count_nonzero((src_shape+sub_shape) == 1)
+        print("Final score: {}".format(score))
+
+        return score
+
+
+    def _normalize_contour(self, img, width=300):
+        "Crop shape and then resize image based on width"
+        # img MUST be binary
+
+        cnt,h = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+        bounding_rect = cv2.boundingRect(cnt[0])
+        img_cropped_bounding_rect = img[bounding_rect[1]:bounding_rect[1] + bounding_rect[3],
+                                    bounding_rect[0]:bounding_rect[0] + bounding_rect[2]]
+
+        new_height = int((1.0 * img.shape[0])/img.shape[1] * width)
+        img_resized = cv2.resize(img_cropped_bounding_rect, (width, new_height))
+
+        return img_resized
+
+
+    def _resize_and_norm_imgs(self, img1_fn, img2_fn, flip_sub, width):
+
         img1 = cv2.imread("{}{}".format(FIGS_PATH,img1_fn), cv2.IMREAD_GRAYSCALE)
         img2 = cv2.imread("{}{}".format(FIGS_PATH,img2_fn), cv2.IMREAD_GRAYSCALE)
 
@@ -55,8 +171,10 @@ class CompareTools(object):
         (thresh, img_bw2) = cv2.threshold(img2, 128, 255,
                                           cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-        resized_img1, cs1, h1 = self._normalize_contour(img_bw1, width)
-        resized_img2, cs2, h2 = self._normalize_contour(img_bw2, width)
+        resized_img1 = self._normalize_contour(img_bw1, width)
+        resized_img2 = self._normalize_contour(img_bw2, width)
+
+
 
         cv2.imshow('tool1', resized_img1)
         cv2.waitKey()
@@ -84,40 +202,13 @@ class CompareTools(object):
 
         # -1 for all contours.
         num_contours = -1
-        shape1 = cv2.drawContours( blank.copy(), cs1,  num_contours,
+        src_shape = cv2.drawContours( blank.copy(), cs1,  num_contours,
                                    1, thickness=-1)
-        shape2 = cv2.drawContours( blank.copy(), cs2, num_contours,
+        sub_shape = cv2.drawContours( blank.copy(), cs2, num_contours,
                                    color=1, thickness=-1)
 
-        cv2.imshow('tool1', shape1)
-        cv2.waitKey()
-        cv2.imshow('tool2', shape2)
-        cv2.waitKey()
 
+        if flip_sub:
+            sub_shape = np.flip(sub_shape)
 
-        # Shapes are drawn in white ( white == 1)
-        shape1_area = np.count_nonzero(shape1)
-        shape2_area = np.count_nonzero(shape2)
-        print("shape 1 area: {} shape 2 area: {}".format(shape1_area, shape2_area))
-
-        # Intersection area == 2, black == 0, contour area == 1
-        score = np.count_nonzero((shape1+shape2) == 1)
-        print("Final score: {}".format(score))
-
-        return score
-
-
-    def _normalize_contour(self, img, width=300):
-        "Crop shape and then resize image based on width"
-        # img MUST be binary
-
-        cnt,h = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-        bounding_rect = cv2.boundingRect(cnt[0])
-        img_cropped_bounding_rect = img[bounding_rect[1]:bounding_rect[1] + bounding_rect[3],
-                                    bounding_rect[0]:bounding_rect[0] + bounding_rect[2]]
-
-        new_height = int((1.0 * img.shape[0])/img.shape[1] * width)
-        img_resized = cv2.resize(img_cropped_bounding_rect, (width, new_height))
-
-        return img_resized, cnt, h
+        return src_shape, sub_shape
