6e87ded98f5de04009824473e576f27fd3bcef36
Jake Brawer <jake.brawer@yale.edu>
2020-06-11 10:07:01 -0400

    Added weighted hamming distance method.

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as Rot
+from mpl_toolkits.mplot3d import Axes3D, art3d
+from sklearn.metrics.pairwise import cosine_similarity
 
 def close_to(m, n, error=1e-6):
     return m >= n - error and m <= n + error
@@ -76,6 +79,46 @@ def min_point_distance(pc1, pc2):
 
     return avg_min_distance
 
+def weighted_min_point_distance(src_pc, sub_pc, src_cntct_pnt, sigma=1):
+    """
+    Same as min_point_distance but weights sub_pc pnts by how far they are
+    from src_cntct_pnt. This is because we really only care about the local
+    properties of the tool near the contact pnt.
+    """
+    length_pc1 = src_pc.shape[1]
+    length_pc2 = sub_pc.shape[1]
+
+    weights = np.apply_along_axis(lambda col: 1.0 - rbf(src_cntct_pnt, col, sigma) ,
+                                  arr=sub_pc, axis=0)
+
+    print("weights")
+
+    # repeat each column length_pc1 times, for example, let length_pc1 = 3
+    # then the array
+    # x = np.array([[1,2],
+    #               [3,4]])
+    # becomes
+    # array([[1, 1, 1, 2, 2, 2],
+    #        [3, 3, 3, 4, 4, 4]])
+    repeated_pc2 = np.repeat(sub_pc, length_pc1, axis=1)
+    # repeat the entire marix and repeat itself length_pc2 times, for example, let length_pc2 = 3
+    # then the array x becomes
+    # array([[1, 2, 1, 2, 1, 2],
+    #        [3, 4, 3, 4, 3, 4]])
+    repeated_pc1 = np.hstack([src_pc] * length_pc2)
+    difference = repeated_pc2 - repeated_pc1
+    all_distance = np.multiply(difference, difference)
+    all_distance = np.sqrt(np.sum(all_distance, axis=0))
+    all_distance = np.reshape(all_distance, (length_pc2, length_pc1))
+    min_distance = np.amin(all_distance, axis=1)
+    min_distance *= weights
+    avg_min_distance = np.mean(min_distance)
+
+    print "avg_min_distance = ", avg_min_distance
+
+    return avg_min_distance
+
+
 def is_2d_point_cloud_overlap(pc1, pc2, threshold):
     """
     pc1: 2 * n
@@ -87,55 +130,150 @@ def is_2d_point_cloud_overlap(pc1, pc2, threshold):
     test_2 = min_point_distance(pc2, pc1) < threshold
     return test_1 and test_2
 
-def r_x(a):
-    return np.array([
-            [1., 0., 0.],
-            [0., np.cos(a), -np.sin(a)],
-            [0., np.sin(a), np.cos(a)]
-        ])
 
 
-def r_y(a):
-        return np.array([
-            [np.cos(a), 0, np.sin(a)],
-            [0, 1, 0],
-            [-np.sin(a), 0,  np.cos(a)]
-        ])
-
-def r_z(a):
-        return  np.array([
-            [np.cos(a), -np.sin(a), 0],
-            [np.sin(a), np.cos(a), 0],
-            [0, 0, 1]
-        ])
-
-def rotation_matrix_from_vectors(vec1, vec2):
-    """ Find the rotation matrix that aligns vec1 to vec2
-    :param vec1: A 3d "source" vector
-    :param vec2: A 3d "destination" vector
-    :return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
+def rbf(x1, x2, sigma = 1):
     """
-    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
+    Radial basis function. Returns vals btwn [0-1].
+    1 is returned if x1 == x2.
+    """
+    diff = x1 - x2
+    return np.exp( -(np.dot(diff, diff)) / (2. * (sigma ** 2)))
+
+
+def rotation_matrix_from_box_rots(sub_dir,src_dir):
+    sub_dir, src_dir = ((sub_dir / np.linalg.norm(sub_dir)).reshape(3),
+                                  (src_dir / np.linalg.norm(src_dir)).reshape(3))
+    v = np.cross(sub_dir, src_dir)
+    c = np.dot(sub_dir, src_dir)
+    theta = np.arccos(c)
+
+    eps = .15
+    is_parallel =  np.isclose(np.linalg.norm(v), np.zeros(1), atol=eps).item()
+    is_same_dir  = np.isclose(theta,  np.zeros(1), atol=eps).item()
+
+    rots = [np.pi /2 , np.pi, 3 * np.pi / 2, 2 * np.pi]
+    scores = []
+
+    if is_parallel and is_same_dir:
+        return np.identity(3)
+    elif not is_parallel:
+        rot_vec = v
+
+    else:
+        a = sub_dir
+        rot_vec = np.array([a[1], -a[0], 0.]) if a[2] < a[0] else np.array([0, -a[2], a[1]])
+
+    for rot in rots:
+        rot_vec *= rot
+        R = Rot.from_rotvec(rot_vec)
+        R = R.as_dcm()
+        rot_sub_dir = R.dot(sub_dir.T)
+
+        scores.append((R, cosine_similarity(rot_sub_dir.reshape(1,-1),
+                                            src_dir.reshape(1,-1))))
+
+    
+    return max(scores, key=lambda score: score[1])[0]
+
+
+def rotation_matrix_from_vectors(a, b):
+    """ Find the rotation matrix that aligns a to b
+    :param a: A 3d "source" vector
+    :param b: A 3d "destination" vector
+    :return mat: A transform matrix (3x3) which when applied to a, aligns it with b.
+    """
+    a, b = (a / np.linalg.norm(a)).reshape(3), (b / np.linalg.norm(b)).reshape(3)
     v = np.cross(a, b)
     c = np.dot(a, b)
+    theta = np.arccos(c)
+
+    eps = .15
+    is_parallel =  np.isclose(np.linalg.norm(v), np.zeros(1), atol=eps).item()
+    is_opp_dir  = np.isclose(theta, np.pi, atol=eps).item()
+    is_same_dir  = np.isclose(theta,  np.zeros(1), atol=eps).item()
+
+    print "V ", v
+    print "IS PARALLEL {}: {} ".format(is_parallel, np.linalg.norm(v))
+    print "THETA ", theta
+    print "SAME DIR: ", is_same_dir
+    print "OPP DIR: ", is_opp_dir
+
+
+    if is_parallel:
+        if is_same_dir:
+            return np.identity(3)
+        elif is_opp_dir:
+            v = np.array([a[1], -a[0], 0.]) if a[2] < a[0] else np.array([0, -a[2], a[1]])
+            v *= np.pi
+            # if not a[2] == 0.:
+            #     v = np.array([1.,1., -(a[0] + a[1]) / a[2]])
+            # elif not a[1] == 0.:
+            #     v = np.array([1.,-(a[0] + a[2]) / a[1], 1.])
+            # else:
+            #     v = np.array([-(a[1] + a[2]) / a[0], 1., 1.])
+            # v = v /np.linalg.norm(v)
+
     s = np.linalg.norm(v)
+    v = v /s
+    print "NEW V ", v
+    # if is_parallel:
+        # rot_vec = np.array([a[1], -a[0], 0.]) if a[2] < a[0] else np.array([0, -a[2], a[1]])
+        # rot_vec *= np.pi
+        # print "ROT VEC ", rot_vec
+        # R = Rot.from_rotvec(rot_vec)
+        # R = R.as_dcm()
+        # # R[:, 0] = -1 * R[:, 0]
+
+    # else:
     kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
-    return rotation_matrix
+    R = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
 
 
-def visualize_two_pcs(pnts1, pnts2):
+    # if R.dot(src_c_side_dir)
+    return R
+
+
+
+def visualize_two_pcs(pnts1, pnts2, s1=None, s2=None):
     """
     Just plots two pointclouds for easy comparison.
     """
+
+
+    faces = {'a':[1,2,6,7],
+             'b':[2,7,3,8],
+             'c':[5,8, 6,7],
+             'd':[1,6,5,0],
+             'e':[5,8,3,2],
+             'f':[0,1,2,3]}
+
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
     print "Pnts 1 mean: {} Pnts 2 mean: {}".format(pnts1.mean(axis=0),
                                                    pnts2.mean(axis=0))
 
+
+    if not s1 is None:
+        bb, s = s1
+        face = [bb[faces[s], :]]
+        side = art3d.Poly3DCollection(face)
+        side.set_color('r')
+        ax.add_collection3d(side)
+
+    if not s2 is None:
+        bb, s = s2
+        face = [bb[faces[s], :]]
+        side = art3d.Poly3DCollection(face)
+        side.set_color('b')
+        ax.add_collection3d(side)
+
+
     ax.axis('equal')
     ax.scatter(xs=pnts1[:, 0], ys=pnts1[:, 1], zs=pnts1[:, 2], c='r')
     ax.scatter(xs=pnts2[:, 0], ys=pnts2[:, 1], zs=pnts2[:, 2], c='b')
+
+
 
     plt.show()
